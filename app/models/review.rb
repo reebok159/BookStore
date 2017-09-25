945d6a83b0fffe6d6a8897ae991cda78c719223b
Eugene V <kokokoi9@mail.ru>
2017-09-25 16:50:56 +0300

    Add some validations to review

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,7 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :book
 
+  validates :title, :text, :rating, presence: true
+
   scope :accepted, -> { where(:accepted => true) }
 end
