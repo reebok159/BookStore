aafe3ae392f2e5c51d4e4de6f0fdd38cc3282617
Eugene V <kokokoi9@mail.ru>
2017-09-25 16:46:11 +0300

    Add credit card validation

--- app/models/credit_card.rb
+++ app/models/credit_card.rb
@@ -1,3 +1,9 @@
 class CreditCard < ApplicationRecord
+  validates :number, :name, :expires, :cvv, presence: true
+  validates :number, with: %r{\A\d{16}\z}
+  validates :name, with: %r{\A[a-zA-Z\s]{0,49}\z}
+  validates :expires, with: %r{\A(0[1-9]|10|11|12)\/\d\d\z}
+  validates :cvv, numericality: { only_integer: true }, format: { with: /\A[0-9]{3,4}\z/ }
+
   belongs_to :order
 end
