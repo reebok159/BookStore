dfdb386967ad2072cbae1cd25ea49aefc72054a2
Eugene V <kokokoi9@mail.ru>
2017-09-26 22:04:30 +0300

    Fixed creditCard validations

--- app/models/credit_card.rb
+++ app/models/credit_card.rb
@@ -1,8 +1,8 @@
 class CreditCard < ApplicationRecord
   validates :number, :name, :expires, :cvv, presence: true
-  validates :number, with: %r{\A\d{16}\z}
-  validates :name, with: %r{\A[a-zA-Z\s]{0,49}\z}
-  validates :expires, with: %r{\A(0[1-9]|10|11|12)\/\d\d\z}
+  validates :number, format: { with: %r{\A\d{16}\z} }
+  validates :name, format: { with: %r{\A[a-zA-Z\s]{0,49}\z} }
+  validates :expires, format: { with: %r{\A(0[1-9]|10|11|12)\/\d\d\z} }
   validates :cvv, numericality: { only_integer: true }, format: { with: /\A[0-9]{3,4}\z/ }
 
   belongs_to :order
