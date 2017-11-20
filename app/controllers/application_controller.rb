1a79be8424d8b5abd3b87a48c84d989032b941d4
Eugene V <kokokoi9@mail.ru>
2017-11-20 13:35:52 +0200

    refactored application_controller (last_order, create_order)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,33 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :last_order, :set_locale
-  before_action :current_ability
+  before_action :last_order, unless: :current_user
+  before_action :set_locale, :current_ability
   helper_method :last_order
-
-  def index
-    render 'layouts/application'
-  end
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
   end
 
-  def set_cart
-    @cart = last_order
-  end
-
   def last_order
-    if current_user.nil?
-      create_order
-      Order.find_by(id: cookies.signed[:order_id], status: :in_progress)
-    else
-      order = current_user.orders.find_by(status: :in_progress)
-      if order.nil?
-        current_user.orders.create
-      else
-        order
-      end
-    end
+    return create_order if current_user.nil?
+    current_user.orders.where(status: :in_progress).first_or_create
   end
 
   def create_order
@@ -39,6 +22,8 @@ class ApplicationController < ActionController::Base
     end
     order = Order.create
     cookies.signed[:order_id] = order.id
+
+    order
   end
 
   private
@@ -46,5 +31,4 @@ class ApplicationController < ActionController::Base
   def current_ability
     @current_ability ||= Ability.new(current_user)
   end
-
 end
