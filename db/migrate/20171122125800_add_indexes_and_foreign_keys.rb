a7cf2a352d55af2561adbf078b415e9fa1e9e21b
Eugene V <kokokoi9@mail.ru>
2018-02-24 12:17:48 +0200

    change migration for adding foreign keys and indexes

--- db/migrate/20171122125800_add_indexes_and_foreign_keys.rb
+++ db/migrate/20171122125800_add_indexes_and_foreign_keys.rb
@@ -1,42 +1,32 @@
 class AddIndexesAndForeignKeys < ActiveRecord::Migration[5.1]
   def change
+    change_table :authors_books do |t|
+      t.belongs_to :book, foreign_key: true
+      t.belongs_to :author, foreign_key: true
+    end
 
-    add_foreign_key :authors_books, :books
-    add_foreign_key :authors_books, :authors
+    change_table :books do |t|
+      t.belongs_to :category, foreign_key: true, index: true
+    end
 
-    add_foreign_key :billing_addresses, :users
-    add_index :billing_addresses, :user_id
+    change_table :credit_cards do |t|
+      t.belongs_to :order, foreign_key: true, index: true
+    end
 
-    add_foreign_key :books, :categories
-    add_index :books, :category_id
+    change_table :order_items do |t|
+      t.belongs_to :order, foreign_key: true, index: true
+      t.belongs_to :book, column: :item_id, foreign_key: true, index: true
+    end
 
-    add_foreign_key :credit_cards, :orders
-    add_index :credit_cards, :order_id
+    change_table :orders do |t|
+      t.belongs_to :user, foreign_key: true, index: true
+      t.belongs_to :delivery_method, foreign_key: true, index: true
+      t.belongs_to :coupon, foreign_key: true, index: true
+    end
 
-    add_foreign_key :info_books, :books
-    add_index :info_books, :book_id
-
-    add_foreign_key :order_addresses, :orders
-    add_index :order_addresses, :order_id
-
-    add_foreign_key :order_items, :orders
-    add_foreign_key :order_items, :books, column: :item_id
-    add_index :order_items, :order_id
-    add_index :order_items, :item_id
-
-    add_foreign_key :orders, :users
-    add_foreign_key :orders, :delivery_methods
-    add_foreign_key :orders, :coupons
-    add_index :orders, :user_id
-    add_index :orders, :delivery_method_id
-    add_index :orders, :coupon_id
-
-    add_foreign_key :reviews, :users
-    add_foreign_key :reviews, :books
-    add_index :reviews, :user_id
-    add_index :reviews, :book_id
-
-    add_foreign_key :shipping_addresses, :users
-    add_index :shipping_addresses, :user_id
+    change_table :reviews do |t|
+      t.belongs_to :user, foreign_key: true, index: true
+      t.belongs_to :book, foreign_key: true, index: true
+    end
   end
 end
