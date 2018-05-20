84ec86cbcda31a792fe30ec466dee5a95bdf58fa
Eugene V <kokokoi9@mail.ru>
2018-05-20 16:56:19 +0300

    small changes, add .env.example

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180313154132) do
+ActiveRecord::Schema.define(version: 20180520133510) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,8 @@ ActiveRecord::Schema.define(version: 20180313154132) do
     t.datetime "updated_at", null: false
     t.string "code"
     t.float "discount", default: 0.0
+    t.integer "type", default: 0
+    t.boolean "activated", default: false
   end
 
   create_table "credit_cards", force: :cascade do |t|
