3ca41764d845bd4b3b3874870cba0aae92efa00e
Eugene V <kokokoi9@mail.ru>
2018-05-20 16:42:50 +0300

    fix ability (problem in rails_admin with users rewrite when update)

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,8 +4,8 @@ class Ability
   def initialize(user)
     user ||= User.new
     can :read, Book
-    can :manage, OrderItem, order: { user_id: user.id }
-    can %i[create update cart], Order, user: user.id
+    can :manage, OrderItem
+    can %i[create update cart], Order
 
     return unless user.persisted?
     can :read, Order, user_id: user.id
