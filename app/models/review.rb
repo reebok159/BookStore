24ec5f6e71dbd24c98ace9fe86bc7a7fe83a8751
Eugene V <kokokoi9@mail.ru>
2018-05-27 22:59:17 +0300

    increase review text length

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,7 +5,7 @@ class Review < ApplicationRecord
   validates_presence_of :title, :text, :rating
   validates :title, :text, format: { with: /\A[0-9A-z!#$%&'\-*,+=?^_`{|}~. ]+\z/ }
   validates :rating, inclusion: { in: 1..5 }
-  validates_length_of :text, maximum: 500
+  validates_length_of :text, maximum: 1000
   validates_length_of :title, maximum: 80
 
   enum status: %i[unprocessed approved rejected]
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Review, type: :model do
 
   describe 'validations' do
     it { is_expected.to validate_presence_of(:text) }
-    it { is_expected.to validate_length_of(:text).is_at_most(500) }
+    it { is_expected.to validate_length_of(:text).is_at_most(1000) }
     it { is_expected.to validate_presence_of(:title) }
     it { is_expected.to validate_length_of(:title).is_at_most(80) }
     it { is_expected.to validate_inclusion_of(:rating).in_range(1..5) }
